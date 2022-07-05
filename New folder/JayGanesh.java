fcf5bf7b8bebfe308cd383018d0b0dfe75ef15b8
Swastik  Deshmukh <91239444+swastik93-dot@users.noreply.github.com>
2022-07-05 18:56:12 +0530

    Add files via upload

--- New folder/JayGanesh.java
+++ New folder/JayGanesh.java
@@ -2,7 +2,7 @@ import java.lang.*;
 
 class JayGanesh
 {
-    Public Static void main(String arg[])  // static cha s small lihala mahnun error ali hoti
+    public static void main(String arg[])  // static cha s small lihala mahnun error ali hoti
     {
         System.out.println("Jay Ganesh...");
 
